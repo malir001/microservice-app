6d93d21407efe1661b84e950e300195db2907c2e
Rahul Mali <115953693+malir001@users.noreply.github.com>
2025-08-29 19:06:55 +0530

    Update AppTest.java

--- src/test/java/com/example/microservice/AppTest.java
+++ src/test/java/com/example/microservice/AppTest.java
@@ -1,4 +1,4 @@
-package com.growingflowers.app;
+package com.example.microservice;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
@@ -7,7 +7,7 @@ public class AppTest {
 
     @Test
     public void testGreet() {
-        App app = new App();
+        App app = new App();  // refers to com.example.microservice.App
         String expected = "Hello, DevOps Engineer!";
         assertEquals(expected, app.greet());
     }
